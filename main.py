c79e1d26f3196de17049cc34df85a686c97fb739
Eren Sezener <erensezener@gmail.com>
2016-01-23 13:45:51 +0100

    improve main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import output_layer
+import input_neuron
 import numpy as np
 
-o = output_layer.OutputLayer(np.zeros((10,400)))
-o.process_all_inputs()
-print o.output_layer_outputs
+
+input_layer_outputs = input_neuron.input_rates(np.zeros((10,400)))
+assert np.shape(input_layer_outputs) == (10,400)
+print 'Input processed'
+output_layer_outputs = output_layer.OutputLayer(input_layer_outputs)
+output_layer_outputs.process_all_inputs()
+print 'Output processed'
+
+print output_layer_outputs.output_layer_outputs
 # print sum(o.get_weights(1))
