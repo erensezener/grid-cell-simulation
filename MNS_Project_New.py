6d7eab1ecc26fa90c9428feea5fda896ad8f6469
Claudia Winklmayr <claudia.winklmayr@gmx.net>
2016-01-23 13:58:01 +0100

    updated rat

--- MNS_Project_New.py
+++ MNS_Project_New.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[30]:
 
 get_ipython().magic(u'matplotlib inline')
 import numpy as np
@@ -28,26 +28,43 @@ def running_rat(t_max, timestep=10.0, velocity=40, x_lim=62.5, y_lim=62.5): #len
     for i in range(int(t_max/timestep)-1):
         phi=np.random.normal(loc=phi, scale=0.2)
         x,y=compute_new_position(x,y,phi,r)
-        k=0
         while abs(x)>=x_lim or abs(y)>=y_lim:
-            if k<10:
-                turn_arg=np.random.randint(0, high=2)
-                phi=phi+turn[turn_arg]                
-            else:
-                phi=phi+(np.pi*0.5)
+            phi=phi+(np.pi*0.5)
             x,y=compute_new_position(x,y,phi,r)     
-            k=k+1
         t=t+timestep       
         output[i+1,0]=x
         output[i+1,1]=y
         output[i+1,2]=t
     return output
 
-rat=running_rat(10000)
-plt.plot(rat[:,0], rat[:,1])
-plt.xlim(-65,65)
-plt.ylim(-65,65)
-plt.show()
+def plot_rat_trajectory(rat_matrix):
+    plt.plot(rat_matrix[:,0], rat_matrix[:,1])
+    plt.xlim(-65,65)
+    plt.ylim(-65,65)
+    plt.show()
+
+def rat2txt(rat_matrix):
+    data= open('Rat_Data.txt', 'w')
+    for i in range(len(rat_matrix[:,1])):
+        data.write(str(rat_matrix[i,0])+ "\t" +str(rat_matrix[i,1])+ "\t" +str(rat_matrix[i,2])+"\n")
+    data.close()
+    
+def rat_txt_to_matrix(filename):
+    data=open(str(filename), 'r')
+    outputmatrix=np.zeros(3)
+    for line in data:
+        values=line.split()
+        v=np.zeros(3)
+        v[0]=float(values[0])
+        v[1]=float(values[1])
+        v[2]=float(values[2])
+        outputmatrix= np.vstack((outputmatrix,v))
+    return outputmatrix
+
+rat=running_rat(4000000)
+plot_rat_trajectory(rat)
+rat2txt(rat)
+#print rat_txt_to_matrix("Rat_Data.txt")
 
 
 # In[ ]:
