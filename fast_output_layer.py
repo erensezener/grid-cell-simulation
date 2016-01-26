de1c113b9a9768c1854512672835a2fc66562760
Eren Sezener <erensezener@gmail.com>
2016-01-26 14:47:00 +0100

    bug fixes again

--- fast_output_layer.py
+++ fast_output_layer.py
@@ -16,10 +16,12 @@ class FastOutputLayer:
         self.input_layer_inputs = input_layer_inputs  # np matrix n by 400
         self.output_layer_outputs = np.zeros((np.size(input_layer_inputs, 0), self.size))  # time-step x 100
         self.max_optim_iters = 100
-        self.activation_fun = lambda g, mu, r_plus: 2.0 / np.pi * np.arctan(g * (r_plus - mu)) * 0.5 * (np.sign(r_plus - mu) + 1)
+
+        # it is not + 1 to avoid zero
+        self.activation_fun = lambda g, mu, r_plus: 2.0 / np.pi * np.arctan(g * (r_plus - mu)) * 0.5 * (np.sign(r_plus - mu) + 1.0000001)
 
         # layer parameters
-        self.weights = np.random.uniform(0, 1, self.network_size)
+        self.weights = 0.9 + 0.1*np.random.uniform(0, 1, self.network_size) #as stated in the paper
         self.normalize_weights()
         self.avg_input_rates = np.zeros((self.input_size,))
         self.avg_firing_rate = np.zeros((self.size,))
@@ -34,7 +36,7 @@ class FastOutputLayer:
         self.tau_plus = .1  # seconds
         self.tau_minus = .3  # seconds
         self.delta_t = 1e-3  # seconds
-        self.debug_mode = True
+        self.debug_mode = False
 
     # computes outputs for each time step from input layer outputs
     def process_all_inputs(self):
@@ -47,26 +49,42 @@ class FastOutputLayer:
             if self.debug_mode:
                 all_a = []
                 all_s = []
+                all_g = []
+                all_mu = []
+            activation_fun_aux = lambda g, mu: self.activation_fun(g, mu, self.r_plus)
             for j in range(self.max_optim_iters):
-                activation_fun_aux = lambda g, mu: self.activation_fun(self.g, self.mu, self.r_plus)
                 output = activation_fun_aux(self.g, self.mu)
+                # if np.any(np.isnan(output)):
+                #     print 'nanalert for output'
                 a = np.mean(output)
                 # s = np.power(a,2) / np.sum(np.power(output,2)) * self.size #same with below but faster
                 s = (np.sum(output) ** 2) / np.sum(np.power(output, 2)) / self.size
                 if self.debug_mode:
                     all_a.append(a)
                     all_s.append(s)
+                    all_g.append(self.g)
+                    all_mu.append(self.mu)
                 if (self.a_bounds[0] < a < self.a_bounds[1]) \
                         and (self.s_bounds[0] < s < self.s_bounds[1]):
                     break
 
                 self.mu += self.b_mu * (a - self.a_0)
-                self.g += self.b_g * self.g * (s - self.s_0)
+                g = self.g + self.b_g * self.g * (s - self.s_0)
+                # if np.isnan(g):
+                #     print 'nanalert for g'
+                self.g = g
 
             if self.debug_mode:
-                plt.plot(all_a)
-                plt.plot(all_s)
+                plt.plot(all_a, label='a')
+                plt.plot(all_s, label='s')
                 plt.show()
+                plt.plot(all_mu, label='mu')
+                plt.legend()
+                plt.show()
+                plt.plot(all_g, label='g')
+                plt.legend()
+                plt.show()
+
 
             output = self.activation_fun(self.g, self.mu, self.r_plus)
             self.output_layer_outputs[i, :] = output
@@ -74,7 +92,7 @@ class FastOutputLayer:
             self.avg_firing_rate += self.eta * (output - self.avg_firing_rate)  # size: 100
             self.avg_input_rates += self.eta * (input - self.avg_input_rates)  # size: 400
             self.weights += self.epsilon * (
-            np.outer(output, input) - np.outer(self.avg_firing_rate, self.avg_input_rates))
+                np.outer(output, input) - np.outer(self.avg_firing_rate, self.avg_input_rates))
             self.normalize_weights()
 
     def normalize_weights(self):
